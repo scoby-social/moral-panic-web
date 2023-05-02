5223d293bcd389d69f3bc7a073e9a35fa8158a3b
Christian Guevara <chrisdb150@gmail.com>
2023-05-02 14:53:04 -0400

    trying some spine animations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: [
+    "@esotericsoftware/spine-player",
+    "@esotericsoftware/spine-core",
+    "@esotericsoftware/spine-webgl",
+  ],
+  images: {
+    domains: ["storage.googleapis.com", "shdw-drive.genesysgo.net"],
+    minimumCacheTTL: 1500000,
+  },
+  crossOrigin: "anonymous",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
