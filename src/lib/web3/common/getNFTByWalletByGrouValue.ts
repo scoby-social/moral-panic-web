cb90fac4eb35c56341e00bd90352f2b7f86555c4
Christian Guevara <chrisdb150@gmail.com>
2023-06-02 12:11:02 -0400

    get compass rose by groupValue

---
+++ src/lib/web3/common/getNFTByWalletByGrouValue.ts
@@ -0,0 +1,18 @@
+import { PublicKey } from "@solana/web3.js";
+
+import WrapperConnection from "../wrappedConnection";
+
+export async function getNFTByWalletByGrouValue(
+    groupKey: string,
+    rpcUrl: string,
+    owner: PublicKey,
+) {
+    const conn = new WrapperConnection(rpcUrl)
+    const rpcAssets = await conn.getAssetsByOwner({ ownerAddress: owner.toBase58() }).then(res => {
+        return res
+    });
+
+    const nfts = rpcAssets.items.find(val => val.grouping.find(i => i.group_value === groupKey));
+
+    return nfts || null;
+}
